5761446562b01aa6626e575e49a47a2563b08732
Josh Little <josh.little@menloschool.org>
2024-08-28 13:51:38 -0700

    Attempt #1

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,12 @@
 Name: YOUR NAME HERE
 
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
+| Date      |     Time      |                                                                                                                                                                            Update |
+|:----------|:-------------:|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Aug 22    |   8-8:30pm    |                                                                                                         I started by thinking about my place in the universe.<br/>It was scary... |
+| Aug 24-26 |   2+ hours    | Thought about the fourth puzzle and tried to solve it in a variety of ways with information gathered via its UTF-8 code converted in binary, hex, and decimal but nothing worked. |
+| Aug 27    |  10-10:30pm   |                                                                       I started working on writing the code to solve the first puzzle and completed it exccluding the apostrophe. |
+| Aug 28    | 6:15 - 6:45am |    Nearly completed both the 2nd and 3rd puzzles with a few minor grammatical errors (such as there not being an extra space after comms) in 15 mins then thought about puzzle 4. |
+
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -4,13 +4,30 @@
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: YOUR NAME HERE
+ * Completed by: Josh Little
  */
 public class Riddler {
 
     public String decryptOne(String encrypted) {
         String decrypted = "";
-
+        for (int i = 0; i < encrypted.length(); i++) {
+            char temp = encrypted.charAt(i);
+            if (temp >= 113 || temp > 81 && temp < 97) {
+                decrypted += (char)(temp - 17);
+            } else if (temp < 65) {
+                decrypted += temp;
+            } else {
+                decrypted += (char)(temp + 9);
+            }
+        }
+        for (int i = 0; i < decrypted.length(); i++) {
+            char temp = decrypted.charAt(i);
+            if (temp < 91 && temp > 64 && i > 1) {
+                System.out.println();
+                System.out.print(temp);
+            } else
+                System.out.print(temp);
+        }
         // TODO: Complete the decryptOne() function.
 
         return decrypted;
@@ -18,7 +35,15 @@ public class Riddler {
 
     public String decryptTwo(String encrypted) {
         String decrypted = "";
-
+        for (int i = 0; i < encrypted.length(); i++) {
+            int temp = 0;
+            while (encrypted.charAt(i + temp) != ' ') {
+                temp++;
+            }
+            decrypted += (char)Integer.parseInt(encrypted.substring(i, i + temp));
+            i += temp;
+        }
+        System.out.println(decrypted);
         // TODO: Complete the decryptTwo() function.
 
         return decrypted;
@@ -26,14 +51,21 @@ public class Riddler {
 
     public String decryptThree(String encrypted) {
         String decrypted = "";
-
+        for (int i = 0; i < encrypted.length() / 8; i++) {
+            decrypted += (char)Integer.parseInt(encrypted.substring(i * 8, (i + 1) * 8), 2);
+        }
         // TODO: Complete the decryptThree() function.
-
+        System.out.println(decrypted);
         return decrypted;
     }
 
     public String decryptFour(String encrypted) {
+        for (int i = 0; i < encrypted.length(); i++)
+        {
+
+        }
         String decrypted = "";
+        encrypted.codePoints().mapToObj(Integer::toHexString).forEach(System.out::println);
 
         // TODO: Complete the decryptFour() function.
 
